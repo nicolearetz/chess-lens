3c1e9b18e2219048c809ed3076a8adcabd2e97b0
nicole <nicole.aretz@t-online.de>
2022-12-29 17:52:06 -0600

    how to load and save images, created conda environment

---
+++ scripts/standardimports.py
@@ -0,0 +1,9 @@
+import sys  # for importing from a different directory
+sys.path.insert(0, '../source')
+sys.path.insert(0, '../images')
+
+import imageio as iio
+import matplotlib.pyplot as plt
+
+import numpy as np
+
