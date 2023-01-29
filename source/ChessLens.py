8711e19b8d0c412d1e87b56af19b1e219dd3f504
nicole <nicole.aretz@t-online.de>
2023-01-28 23:44:19 -0600

    improved cutting in the identification of the physical board

--- source/ChessLens.py
+++ source/ChessLens.py
@@ -64,59 +64,90 @@ class ChessLens():
         if self.img_active != self.img_canny:
             raise RuntimeError("ChessLens.identify_physical called when active image is not the outline image")
 
+        # rescale image to only contain zeros and ones, such that the norm is indeed the number of pixels
         img = self.img_active.img / 255
-        bool_cut_rows = True
-        bool_cut_cols = True
+        bool_cut_left = True
+        bool_cut_right = True
+        bool_cut_top = True
+        bool_cut_bottom = True
+        reduction = 0.5
 
-        while bool_cut_cols or bool_cut_rows:
+        while any([bool_cut_left, bool_cut_right, bool_cut_top, bool_cut_bottom]):
 
-            img, bool_cut_rows = self.cut_image(img, axis=1, min_pixels=min_pixels)
-            img, bool_cut_cols = self.cut_image(img, axis=0, min_pixels=min_pixels)
+            img, bool_cut_left = self.restrict_image_from_side(img, axis=0, direction=1, min_pixels=min_pixels, max_reduction=reduction)
+            img, bool_cut_top = self.restrict_image_from_side(img, axis=1, direction=1, min_pixels=min_pixels,
+                                                               max_reduction=reduction)
+            img, bool_cut_right = self.restrict_image_from_side(img, axis=0, direction=-1, min_pixels=min_pixels,
+                                                               max_reduction=reduction)
+            img, bool_cut_bottom = self.restrict_image_from_side(img, axis=1, direction=-1, min_pixels=min_pixels,
+                                                              max_reduction=reduction)
+            reduction /= 2
 
-        self.img_cropped_to_physical = ChessImage(img, type="physical", parent_image=self.img_canny)
+        self.img_cropped_to_physical = ChessImage(img * 255, type="physical", parent_image=self.img_canny)
         return self.img_cropped_to_physical
 
-    def cut_image(self, img, axis, min_pixels=2):
-        """finds the first row (axis = 1) or column (axis = 0) index for which the image contains less than min_pixels.
-        Then finds the next entry for which the row / column has more than min_pixels entries again.
-        Then cuts along both rows / columns and keeps the outside one with the larger number of pixels.
+    def restrict_image_from_side(self, img, axis, direction, min_pixels=2, max_reduction=0.1):
+        """
+        going from one of the four sides, this function determines the furthest position inward it can cut the image
+        such that the row / column one pixel further outward has strictly less than <min_pixels> pixels. Returns the
+        original imagee if no such row / column was found. The sides can be specified as follows:
+
+        cut from left: axis = 0, direction = 1
+        cut from right: axis = 0, direction = -1
+        cut from top: axis = 1, direction = 1
+        cut from bottom: axis = 1, direction = -1
+
+        The function only looks so far inward that the image height / width is not reduced by more than
+        <max_reduction>*100%.
+
+        :param img: image to be cut, scaled such that entries are between 0 and 1
+        :param axis: 0 for a vertical cut, 1 for a horizontal cut
+        :param direction: 1 or -1
+        :param min_pixels: pixel threshold to consider a column or row empty
+        :param reduction: restriction to how much of the image may be cut off
+        :return:
         """
 
+        if np.max(img) > 1 or np.min(img) < 0:
+            raise RuntimeError("In ChessLens.restrict_image_from_side: img not scaled to entries within [0, 1]")
+
+        if axis not in [0, 1]:
+            raise RuntimeError("In ChessLens.restrict_image_from_side: invalid axis (received axis={})".format(axis))
+
+        if direction not in [1, -1]:
+            raise RuntimeError("In ChessLens.restrict_image_from_side: received invalid direction {}".format(direction))
+
         # compute how many pixels are on each row (if axis = 1) or each column (if axis = 0)
-        norms = la.norm(img, axis=axis)
+        norms = la.norm(img, axis=axis, ord=1)
+        len = norms.shape[0]
 
-        # identify first almost-zero entry
-        zero_index = (norms < min_pixels).argmax()
-
-        if zero_index == 0 and norms[0] >= min_pixels:
-            # no almost-empty row (axis = 1) or column (axis = 0) was found
+        # determine how many rows / columns we actually need to look at
+        scrutiny = int(max_reduction * len)
+        if scrutiny <= 2:
             return img, False
 
-        non_zero_index = (norms[zero_index:] >= min_pixels).argmax()
-        if non_zero_index == 0 and norms[zero_index + non_zero_index] < min_pixels:
-            # no entry right of zero_index is above min_pixels
-            non_zero_index = -1
+        # restrict norms to the considered rows / columns
+        norms = norms[::direction]
+        norms = norms[:scrutiny+1]
+
+        # identify last almost-zero index
+        zero_index = scrutiny - 1
+        while norms[zero_index] >= min_pixels:
+            zero_index -= 1
+            if zero_index == -1:
+                # every norm entry is >= min_pixels
+                return img, False
+        non_zero_index = zero_index + 1
+
+        # identify new starting and stopping indices
+        start = np.max([0, direction * non_zero_index])
+        stop = np.min([len, len + direction * non_zero_index])
+
+        if axis == 0:
+            # cutting from left or right
+            return img[:, start:stop], True
         else:
-            non_zero_index += zero_index
-
-        if axis == 1:
-            # cut the rows
-            top = img[:zero_index, :]
-            bottom = img[non_zero_index:, :]
-        else:
-            # cut the columns
-            top = img[:, :zero_index]
-            bottom = img[:, non_zero_index:]
-
-        # make sure not to discard the majority of information
-        if la.norm(top) + la.norm(bottom) < 0.9 * la.norm(img):
-            return img, False
-
-        # keep image part with the most pixels (= the most information)
-        if la.norm(top) > la.norm(bottom):
-            return top, True
-        else:
-            return bottom, True
-
+            # cutting from top or bottom
+            return img[start:stop, :], True
 
 
